8b2f0fceb44b818a314ac9454a1d1a8f6c7b67ed
mohamedsenetor2024 <mohamedmuhumed001@gmail.com>
2025-07-20 19:56:49 +0300

    Add: my-company React project

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
-import Contact from './pages/Contact'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 function App() {
