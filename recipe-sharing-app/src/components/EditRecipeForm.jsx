5deb752562ecc71bf59958d1da3f237b0c352c7d
mohamedsenetor2024 <mohamedmuhumed001@gmail.com>
2025-07-30 18:20:15 +0300

    Add detailed recipe management: view, edit, delete with Zustand

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -7,7 +7,7 @@ const EditRecipeForm = ({ recipe }) => {
   const [description, setDescription] = useState(recipe.description);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    event.preventDefault();
     updateRecipe({ ...recipe, title, description });
   };
 
