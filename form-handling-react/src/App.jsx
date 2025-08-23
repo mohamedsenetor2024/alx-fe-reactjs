96a733ccf1a357eafa8091df6679fdf204f58ec0
mohamedsenetor2024 <mohamedmuhumed001@gmail.com>
2025-08-23 11:05:23 +0300

    feat: implement form handling in React using controlled components and Formik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FormikForm from './components/FormikForm.js'
+import FormikForm from './components/formikForm.js'
 import RegistrationForm from './components/RegistrationForm'
 
 function App() {
